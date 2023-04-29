71bae80137a283aa6bada97c3716ac1d7233f25f
Roman Brauer <bulick27kom@gmail.com>
2023-04-29 17:36:52 +0300

    03 done

--- 03-arrow-function-arguments/index.ts
+++ 03-arrow-function-arguments/index.ts
@@ -2,17 +2,17 @@ const isBirthdayData: boolean = true;
 let ageData: number = 40;
 const userNameData: string = 'John';
 
-function logBrtMsg(isBirthday: boolean, userName: any, age: number): string {
+const logBrtMsg = (isBirthday: boolean, userName: any, age: number): string => {
   if (isBirthday) {
     //console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
     return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
   } else {
     return 'error';
   }
-}
+};
 
 // logBrtMsg(); // не запустится без аргументов
-logBrtMsg(isBirthdayData, userNameData, ageData);
+// logBrtMsg(isBirthdayData, userNameData, ageData);
 
 const rez = logBrtMsg(true, 'Vasya', 34);
 console.log(rez);
