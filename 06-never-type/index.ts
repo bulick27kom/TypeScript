b844da1469b4fd6e2c8aa7d7b53020b64502b459
Roman Brauer <bulick27kom@gmail.com>
2023-04-30 19:45:39 +0300

    done again

--- 06-never-type/index.ts
+++ 06-never-type/index.ts
@@ -3,16 +3,16 @@ let ageData: number = 40;
 const userNameData: string = 'John';
 
 const createError = (message: string) => {
-  throw new Error(message);
+    throw new Error(message);
 };
 
 function logBrtMsg(isBirthday: boolean, userName: any, age: number): string {
-  if (isBirthday) {
-    //console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
-    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
-  } else {
+    if (isBirthday === true) {
+        return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+    } else if (isBirthday === false) {
+        return 'Too bad';
+    }
     return createError('Error');
-  }
 }
 
 logBrtMsg(isBirthdayData, userNameData, ageData);
