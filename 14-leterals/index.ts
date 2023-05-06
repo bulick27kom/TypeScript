213389467f8e0ca0672a99ee5a1f41c8440252b9
Roman Brauer <bulick27kom@gmail.com>
2023-05-06 13:45:06 +0300

    14 done

--- 14-leterals/index.ts
+++ 14-leterals/index.ts
@@ -1,7 +1,39 @@
-const isBirthday: boolean = true;
-let age: number = 40;
-const userName: string = 'John';
+let msg: 'Hello' = 'Hello';
 
-if (isBirthday) {
-  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+let msg2: 'Hello' | 'world' = 'Hello';
+
+const port3000: number = 3000;
+const port3001: number = 3001;
+
+function startServer(
+    protocol: 'http' | 'https',
+    port: 3000 | 3001
+): 'Server started' {
+    if (port === port3000 || port === port3001) {
+        console.log(`Server started on ${protocol}://server:${port}`);
+    } else {
+        console.error('Invalid port');
+    }
+    return 'Server started';
 }
+
+startServer('http', 3001);
+
+function createAnimation(
+    id: string | number,
+    animationName: string,
+    timingFunction: 'ease' | 'ease-out' | 'ease-in' = 'ease',
+    duration: number,
+    iterationCount: number | 'infinite'
+): void {
+    // const elem = document.querySelector(`#${id}`) as HTMLElement;
+    // if (elem) {
+    console.log(
+        `${animationName} ${timingFunction} ${duration} ${iterationCount}`
+    );
+
+    //    elem.style.animation = `${animationName} ${timingFunction} ${duration} ${iterationCount}`;
+    // }
+}
+
+createAnimation('1', 'rotation', 'ease-in', 3, 'infinite');
