325c5eab141c4bb9fc53329b33eb03b2abf45069
Roman Brauer <bulick27kom@gmail.com>
2023-05-09 15:55:35 +0300

    23 done

---
+++ 23-enum/index.ts
@@ -0,0 +1,29 @@
+const TOP = 'Top';
+const RIGHT = 'Right';
+
+enum Directions {
+    TOP,
+    RIGHT,
+    LEFT,
+    BOTTOM,
+}
+
+enum TimingFunction {
+    EASE = 'ease',
+    EASE_IN = `${EASE}-in`,
+    LINEAR = 'linear',
+}
+
+enum TimingFunctionN {
+    EASE = 3,
+    EASE_IN = 5,
+    LINEAR = EASE * 2,
+}
+
+function frame(element: string, dir: Directions, tFunc: TimingFunction): void {
+    if (dir === Directions.RIGHT) {
+        console.log(tFunc);
+    }
+}
+
+frame('id', Directions.RIGHT, TimingFunction.LINEAR);
