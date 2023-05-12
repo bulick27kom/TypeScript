31d3327fdebb8f3063126b11df5b8e30a741c82b
Roman Brauer <bulick27kom@gmail.com>
2023-05-12 15:06:40 +0300

    32

--- 32-practice/index.ts
+++ 32-practice/index.ts
@@ -1,7 +1,50 @@
-const isBirthday: boolean = true;
-let age: number = 40;
-const userName: string = 'John';
+type Animal = 'cat' | 'dog' | 'bird';
 
-if (isBirthday) {
-  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+enum AnimalStatus {
+    Available = 'available',
+    NotAvailable = 'not available',
+}
+
+interface IAnimalData {
+    animal: Animal;
+    breed: string;
+    sterilized?: string;
+}
+
+interface IAnimalAvailableData extends IAnimalData {
+    location: string;
+    age?: number;
+}
+
+interface IAnimalNotAvailableData {
+    message: string;
+    nextUpdateIn: Date;
+}
+
+interface IAnimalAvailableResponse {
+    status: AnimalStatus.Available;
+    data: IAnimalAvailableData;
+}
+
+interface IAnimalNotAvailableResponse {
+    status: AnimalStatus.Available;
+    data: IAnimalNotAvailableData;
+}
+
+type Resp = IAnimalAvailableResponse | IAnimalNotAvailableResponse;
+
+function isAvailable(responce: Resp): responce is IAnimalAvailableResponse {
+    if (responce.status === AnimalStatus.Available) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkAnimalData(animal: Resp): IAnimalAvailableData | string {
+    if (isAvailable(animal)) {
+        return animal.data;
+    } else {
+        return `${animal.data}, you can try in ${animal.data.nextUpdateIn}`;
+    }
 }
