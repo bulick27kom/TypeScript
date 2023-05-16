763677798b07a0fcaa7f62aca3240c07ab420540
Roman Brauer <bulick27kom@gmail.com>
2023-05-16 14:24:00 +0300

    33

--- 33-working-with-DOM/index.ts
+++ 33-working-with-DOM/index.ts
@@ -1,2 +1,19 @@
-const box = document.querySelector('.box') as HTML
-box.textContent = 'asdad'
+const box = document.querySelector('.box') as HTMLElement;
+// box.textContent = 'asdad'
+const input = document.querySelector('input');
+input?.value;
+
+const link = document.querySelector('a');
+if (link) {
+    link.href = 'asdsa';
+}
+
+const p = document.querySelector('.paragraph') as HTMLParagraphElement;
+
+const links = document.querySelectorAll('a');
+
+const elem = document.createElement('a');
+
+link?.addEventListener('click', (e) => {
+    e.preventDefault();
+});
