bcabb8b8383d886d1a56e4c0bb30e37f41e71d0e
Roman Brauer <bulick27kom@gmail.com>
2023-05-26 12:10:35 +0300

    40 done

--- 40-built-in-generics/index.ts
+++ 40-built-in-generics/index.ts
@@ -1,7 +1,32 @@
-const isBirthday: boolean = true;
-let age: number = 40;
-const userName: string = 'John';
+const arr: Array<number> = [1, 2, 3];
 
-if (isBirthday) {
-  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+const arr1: number[] = [1, 2, 3];
+
+const roarr: ReadonlyArray<string> = ['dfgdf', 'dfgdfg', 'dfgdfg'];
+//roarr[0] = 'qweqwe' //будет ошибка
+
+interface IState {
+    data: {
+        name: string;
+    };
+    tag?: string;
+}
+
+const state: Partial<IState> = {
+    data: {
+        name: 'John',
+    },
+};
+
+const strictState: Required<IState> = {
+    data: {
+        name: 'asdasd',
+    },
+    tag: 'asas',
+};
+
+function action(state: Readonly<IState>) {
+    // state.data = 'aasdasdasd';
+    state.data.name = 'asdasd';
+    // state.tag = 'tag';
 }
