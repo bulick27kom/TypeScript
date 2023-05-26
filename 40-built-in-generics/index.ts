1f21eddca97f047817bac2e67875de4e1262e7e7
Roman Brauer <bulick27kom@gmail.com>
2023-05-26 09:20:41 +0300

    40 init

---
+++ 40-built-in-generics/index.ts
@@ -0,0 +1,7 @@
+const isBirthday: boolean = true;
+let age: number = 40;
+const userName: string = 'John';
+
+if (isBirthday) {
+  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+}
